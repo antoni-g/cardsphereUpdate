4db031913885afd45a921dc450b2bcb2e751f1b2
Antoni <antonig@sas.upenn.edu>
2018-02-26 17:26:42 -0500

    cleaning up repo

---
+++ popup.js
@@ -0,0 +1,70 @@
+// method taken from chrome extension tutorial
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+  chrome.tabs.query(queryInfo, (tabs) => {
+    var tab = tabs[0];
+    var url = tab.url;
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+    callback(url);
+  });
+}
+
+// autosave functionality
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.func === 'autosave') {
+        savePackages();
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	getCurrentTabUrl((tgt) => {
+    	var textSpace = document.getElementById('text');
+    	if (!(tgt === 'https://www.cardsphere.com/send')) {
+    		// display error message if not on the cardsphere site
+    		var error ='<span>Not on Cardsphere\'s send page! Load it to use this extension.</span><br>';
+    		textSpace.innerHTML += error;
+    	} else {
+    		// else show the correct buttons when on the cardsphere site
+    		document.getElementById('buttons').style.display = 'block';
+            $("#save").click(function() {
+                savePackages();
+            });
+            $("#disp_mem").click(function() {
+                printMem();
+            });
+            $("#options").click(function() {
+                chrome.runtime.openOptionsPage();
+            });
+    	}
+    });
+});
+
+function savePackages() { 
+    // send message to save.js content script
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {func: "save"}, function(response) {
+            console.log('Saved.');
+        });
+    });
+
+    var d = new Date();
+    document.getElementById('alert').style.display = 'block';
+    $('#alert').text('Saved! At '+dateFormat(d.toString(), "dddd, mmmm dS, yyyy, h:MM:ss TT"));
+    $('#alert').hover(function() {
+        $(this).css('color', 'red');
+    });
+    $('#alert').click(function() {
+        document.getElementById('alert').style.display = 'none';
+    });
+}
+
+// helper method
+function printMem() {
+    console.log('printing mem');
+    chrome.storage.local.get(null, function(res) {
+        console.log(res);
+    });
+}
---
+++ save.js
@@ -0,0 +1,32 @@
+var settings;
+var saved = {};
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  if (msg.func === 'save') {
+    $(document).ready(function () {
+      save();
+    });
+  }
+});
+
+function save() {
+  $(".package").each(function(index,value) {
+    var heading = $(value).find(".package-heading");
+    var username = $($(heading).children()[0]).find("a").text();
+    var price = $($(heading).children()[1]).find("strong").text();
+    var efficiency = $($(heading).children()[1]).find(".efficiency-index").text();
+    efficiency = efficiency.trim().split(" ")[0].replace(/\n/g, '');
+    var contents =  prepContents($(value).find(".package-body").text());
+    saved[username] = {'price': price, 'efficiency': efficiency,'contents': contents};
+  });
+  // store settings
+  settings = JSON.stringify(getSettings()).hashCode().toString();
+  chrome.storage.local.set({'saved': saved}, function() {});
+  chrome.storage.local.set({[settings]: saved}, function() {});
+  var d = new Date();
+  dateFormat(d, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+  chrome.storage.local.set({'saved_last_accessed': d.toString()}, function() {});
+  var time = settings+'_last_accessed'
+  chrome.storage.local.set({[time]: d.toString()}, function() {});
+  console.log('Saved., yes!');
+}
